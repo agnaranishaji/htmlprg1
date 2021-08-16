661ce3b491772091d5fdafe1b976c67396dcfa8f
Agna Rani Shaji <88897562+agnaranishaji@users.noreply.github.com>
2021-08-16 23:44:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let a=0
+function add()
+{
+    a++
+    document.getElementById('screen').innerText=a
+}
+function sub()
+{
+    a--
+    document.getElementById('screen').innerText=a
+}
+function RESET()
+{
+    a=0
+    document.getElementById('screen').innerText=a
+}
